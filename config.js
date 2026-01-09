941b4c3640491d2ecd7e8712b756d3fdc73a1104
departmentofaimlhkbk-boop <departmentofaiml.hkbk@gmail.com>
2026-01-09 17:58:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dmNtcXZjd2t6cmdzZXhwbmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDk4MDAsImV4cCI6MjA3MjM4NTgwMH0.uVbUY27Lgkj7QGDliGPlWiE3QAhbzG3DiCiPOFYUWrI";
 
 // ✅ IMPORTANT: use a different variable name to avoid conflict
-const supabaseClient = window.supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
